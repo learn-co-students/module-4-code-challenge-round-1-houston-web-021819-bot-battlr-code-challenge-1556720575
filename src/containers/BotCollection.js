dd9cb6fd56102d827de282fdf4ec661cae7a9b7b
moranja <jeffadammoran@gmail.com>
2019-05-01 10:14:30 -0500

    full functionality

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,14 +1,36 @@
 import React from "react";
-import BotCard from "../components/BotCard";
+import BotCard from "../components/BotCard"
+import BotSpecs from "../components/BotSpecs"
 
 class BotCollection extends React.Component {
 
+  state = {
+    selectedBot: {}
+  }
+
+  showSpecs = (id) => {
+    const bot = this.props.bots.find(bot => bot.id === id)
+    this.setState({
+      selectedBot: bot
+    })
+  }
+
+  goBack = () => {
+    this.setState({
+      selectedBot: {}
+    })
+  }
+
   render(){
-    console.log(this.props.bots)
+    console.log(!Object.keys(this.state.selectedBot).length)
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {this.props.bots.map(bot => <BotCard {...bot} key={bot.id} handleClick={this.props.enlist} />)}
+    		  {
+            !Object.keys(this.state.selectedBot).length
+            ? this.props.bots.map(bot => <BotCard {...bot} key={bot.id} handleClick={this.showSpecs}/>)
+            : <BotSpecs {...this.state.selectedBot} key={this.state.selectedBot.id} goBack={this.goBack} enlist={this.props.enlist} />
+          }
     		  Collection of all bots
     		</div>
   	  </div>
